254648cbee0392ff5a30100bb407f22ea430fe8b
Eduardo <jecruxiz@gmail.com>
2020-02-11 14:52:09 -0600

    :sparkles: Introucing bubble_sort
    Introducing bubble sort for strings and numbers

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,8 @@
 test_array = [4,3,78,2,999,6,0,2]
 test_string = ["hi","hello","hey"]
 
+
+
 def bubble_sort_by(array)
     max = 2
     temp_array = array
@@ -16,4 +18,15 @@ def bubble_sort_by(array)
     end
     temp_array
 end
+
+def bubble_sort (array)
+    if array[0].is_a? String
+       a = bubble_sort_by(array) do |left,right| left.length - right.length ;end
+    else
+        a = bubble_sort_by(array) do |left,right| left <=> right ;end
+    end
+    return a 
+end
+
+p bubble_sort(test_string)
 
